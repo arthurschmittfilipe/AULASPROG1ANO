e51e02fa7569c946a65b1c4ef12c6c2d1de10792
arthurschmittfilipe <164406267+arthurschmittfilipe@users.noreply.github.com>
2024-03-21 16:42:43 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,8 @@
+#definição de múltiplas variáveis com valores diferentes
+x, y, z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+#captação de input do usuário
+nome = str(input("olá, qual o seu nome?"))
+print("Olá,", nome)
