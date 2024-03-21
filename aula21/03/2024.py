4daefe42ee668b9117eb0d7c431cef6d6751feb8
arthurschmittfilipe <164406267+arthurschmittfilipe@users.noreply.github.com>
2024-03-21 16:43:42 -0300

    Add files via upload

---
+++ aula21/03/2024.py
@@ -0,0 +1,13 @@
+#Mudanças de tipo de uma variavel
+a = "Agora eu entendi tudo"
+b = "graças ao meu querido professor Big roger"
+print(a+b)
+print(b)
+#Definindo uma variável com base no valor atual de outro
+c = a+b
+print(c)
+#Definições de múltiplas variável com valores iguais
+x = y = z = 10
+print(x)
+print(y)
+print(z)
