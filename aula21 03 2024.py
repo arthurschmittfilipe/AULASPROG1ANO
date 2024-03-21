3737706b102390cf5649e4c955e3faeaee2413ab
arthurschmittfilipe <164406267+arthurschmittfilipe@users.noreply.github.com>
2024-03-21 16:42:31 -0300

    Add files via upload

---
+++ aula21 03 2024.py
@@ -0,0 +1,12 @@
+#variaveis
+#definição das primeiras variaveis e constantes
+a = 3
+b = 7
+c= (a+b)-(b+a)
+print (c)
+print ((a+b)-(b+a))
+print((a-b)*(a+c))
+print((a-c)*(a+b))
+print(a==b or b==c)
+
+
